d93838c553524f3e9c1eb52bd0cce844e38bd67b
Memo-creator <mamounelamin2000@outlook.com>
2026-04-08 16:56:57 -0400

    Referencing Create Help Request header  - start help request model

--- api/DB.hpp
+++ api/DB.hpp
@@ -1,7 +1,7 @@
+#include <pqxx/pqxx>
 #include <string>
 #include <vector>
 #include "AssignmentModel.hpp"
-#include <pqxx/pqxx>
 
 #include "UserModel.hpp"
 
@@ -10,19 +10,21 @@ class DB {
     DB();
     static std::string connectionString;
     pqxx::connection connection;
+
 public:
-    static DB& getInstance();
-    DB(const DB&) = delete;
-    DB& operator=(const DB&) = delete;
-    pqxx::connection&  getConnection();
-	std::vector<Assignment> getAllAssignments();
+    static DB &getInstance();
+    DB(const DB &) = delete;
+    DB &operator=(const DB &) = delete;
+    pqxx::connection &getConnection();
+    std::vector<Assignment> getAllAssignments();
     std::optional<UserModel> getUserByID(int id);
-    std::optional<UserModel> getUserByEmail(std::string& email);
+    std::optional<UserModel> getUserByEmail(std::string &email);
     std::vector<UserModel> getAllUsers();
     bool createUser(UserModel &userModel);
     bool deleteUser(UserModel &userModel);
     bool createAssignment(Assignment &assignment);
     bool deleteAssignment(Assignment &assignment);
-	std::optional<Assignment> getAssignmentByID(int id);
-
+    bool createHelpRequest(HelpRequest &helpRequest);
+    bool deleteHelpRequest(HelpRequest &helpRequest);
+    std::optional<Assignment> getAssignmentByID(int id);
 };
