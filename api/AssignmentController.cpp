f1e298e702abeb5b333b25c512a21f0c1a721607
Farhiya <fahmed1747@conestogac.on.ca>
2026-04-11 13:30:51 -0400

    Added assignment feature - Farhiya
    Refactored createAssignment to return AssignmentResultPayload and added updateAssignment function for modifying existing assignments.

--- api/AssignmentController.cpp
+++ api/AssignmentController.cpp
@@ -1,14 +1,45 @@
 #include "AssignmentController.hpp"
-#include "AssignmentModel.cpp"
+#include "AssignmentModel.hpp"
+#include "DB.hpp"
 
-Assignment AssignmentController::createAssignment(const std::string& title, const std::string& description, int courseId, int userId) {
-	//do validations here
-
+AssignmentResultPayload AssignmentController::createAssignment(const std::string& title, const std::string& description, int courseId, int userId) {
+	
+	
 	Assignment assignment = Assignment();
 	assignment.setTitle(title)
 			  .setDescription(description)
 			  .setCourseId(courseId)
 			   .setPriority(1); // default priority
+	DB& db = DB::getInstance();
+	bool result = db.createAssignment(Assignment);
+	return { 
+		result, 
+		result? "" : "Failed to create assignment",	 
+		result? assignment: std::nullopt
+	};
 
 	return assignment;
+}
+AssignmentResultPayload updateAssignment(int id, const std::string& title, const std::string& description, int priority){
+	//get DB instance
+	DB& db = DB::getInstance();
+	std::optional<Assignment> assignment = db.getAssignmentByID(id);
+	//if it doesn't exist, return error
+	if (!assignment) {
+		return {
+			false,
+			"Assignment not found",
+			std::nullopt
+		};
+	}
+	assignment.setTitle(title)
+			  .setDescription(description)
+			.setPriority(priority);
+	assignment.save();
+	return {
+		true,
+		"",
+		assignment
+	};
+		
 }
